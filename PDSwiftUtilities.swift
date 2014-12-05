e01c3664f8b2e10e03eef69c0590019a1badc11f
Peter Denniss <peter.denniss@gmail.com>
2014-12-05 15:40:49 +0900

    added kbSizeFromNotification method

---
+++ PDSwiftUtilities.swift
@@ -0,0 +1,36 @@
+//
+//  PDSwiftUtilities.swift
+//  animeinterest
+//
+//  Created by Peter Denniss on 5/12/2014.
+//  Copyright (c) 2014 Tattva Inc. All rights reserved.
+//
+
+import UIKit
+
+class PDSwiftUtilities: NSObject
+{
+    class func kbSizeFromNotification(notification: NSNotification, view: UIView) -> CGSize
+    {
+        var kbSize: CGSize?
+        
+        if let info = notification.userInfo
+        {
+            if let keyboardFrame = info[UIKeyboardFrameEndUserInfoKey]?.CGRectValue()
+            {
+                var convertedFrame = view.convertRect(keyboardFrame, fromView: view.window)
+                kbSize = convertedFrame.size
+            }
+        }
+        
+        if kbSize != nil
+        {
+            return kbSize!
+        }
+        else
+        {
+            return CGSizeZero
+        }
+    }
+   
+}
